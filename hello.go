a8ebd56ea11152dab11c848a799217b6894a85f0
Stephane <stephstephg@gmail.com>
2017-07-18 19:03:30 +0200

    adding test for Hash

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"learn-go/util"
 )
@@ -10,7 +11,27 @@ func main() {
 	message := "hello"
 	dh := util.Hash(message)
 
-	fmt.Println(message)
-	fmt.Printf(">%x<\n", dh)
+	fmt.Println("message : " + message)
+	fmt.Printf("hash : >%x<\n", dh)
+
+	for i := range dh {
+		fmt.Printf(" %x ", dh[i])
+	}
+	fmt.Println("temporary byte array : ")
+
+	var tmp = []byte{0x95, 0x95, 0xc9}
+	fmt.Printf("temp : "+">%x<\n", tmp)
+
+	sl := dh[:3]
+	fmt.Printf("slicing hash : >%x<\n", sl)
+
+	if bytes.Equal(sl, tmp) {
+		fmt.Println("SAME")
+	}
+
+	// printing the full byte array
+	for j := range dh {
+		fmt.Printf("@x%x, ", dh[j])
+	}
 
 }
