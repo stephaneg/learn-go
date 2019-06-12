8fff56aa66260f7c7ac602e0986a259000ad3af4
Steph <stephstephg@gmail.com>
2019-06-12 22:57:52 +0200

    complete sdl2 setup

--- sdl2/sdl2.go
+++ sdl2/sdl2.go
@@ -6,8 +6,24 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const winWidth, winHeight int = 800, 600
+
+type color struct {
+	r, g, b byte
+}
+
+func setPixel(x, y int, c color, pixels []byte) {
+	index := (y*winWidth + x) * 4
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
+	}
+}
+
 func main() {
-	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+
+	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
 
 	if err != nil {
 		fmt.Println(err)
@@ -23,7 +39,25 @@ func main() {
 	}
 	defer renderer.Destroy()
 
-	// Biggame loop
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tex.Destroy()
+
+	pixels := make([]byte, winWidth*winHeight*4)
+
+	for y := 0; y < winHeight; y++ {
+		for x := 0; x < winWidth; x++ {
+			setPixel(x, y, color{byte(x % 255), byte(y % 255), 0}, pixels)
+		}
+	}
+
+	tex.Update(nil, pixels, winWidth*4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
 	sdl.Delay(3000)
 
 }
