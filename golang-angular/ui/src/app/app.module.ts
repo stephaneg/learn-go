dca07648ce698d625c4c1f04d265c4ee70760293
stephstephg <stephstephg@gmail.com>
2020-09-06 19:44:33 +0200

    starting angular

--- golang-angular/ui/src/app/app.module.ts
+++ golang-angular/ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
